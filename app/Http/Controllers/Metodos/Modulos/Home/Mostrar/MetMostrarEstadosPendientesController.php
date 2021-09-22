7a00b7f1170fe526e22d30fd253b732714175358
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-09-22 00:12:50 -0500

    mostrar home

--- app/Http/Controllers/Metodos/Modulos/Home/Mostrar/MetMostrarEstadosPendientesController.php
+++ app/Http/Controllers/Metodos/Modulos/Home/Mostrar/MetMostrarEstadosPendientesController.php
@@ -13,6 +13,9 @@ class MetMostrarEstadosPendientesController extends Controller
 {
     public function MetMostrarEstadosPendientes(Request $request)
     {
+        $respuesta = true;
+        $mensaje = "";
+
         $date1 = "";
         $date2 = "";
         
@@ -22,18 +25,11 @@ class MetMostrarEstadosPendientesController extends Controller
         $fechaInicio = $request['fechaInicio'];
         $fechaFinal  = $request['fechaFinal'];
         
-
-        // if($fechaInicio != null){
-        //     $fechaInicio = date("Y-m-d", strtotime($fechaInicio));
-            
-        // }
         
         if($fechaFinal != null){
             $fechaFinal = date("Y-m", strtotime($fechaFinal));
         }else{
-            // $fechaInicio = "2021-07-01";
-            // $fechaFinal = "2021-07-01";
-            $fechaFinal = date("Y-m", strtotime("2021-07-01"));
+            $fechaFinal = null;
         }
 
         $tprs = tprtipospromociones::get();
@@ -45,10 +41,11 @@ class MetMostrarEstadosPendientesController extends Controller
                                         ->join('fecfechas as fec', 'fec.fecid', 'espestadospendientes.fecid')
                                         ->where('tpr.tprid', $tpr->tprid)
                                         ->where(function ($query) use($fechaInicio, $fechaFinal) {
-                                            // if($fechaInicio != null){
-                                                // $query->whereBetween('fecfecha', [$fechaInicio, $fechaFinal]);
+                                            if($fechaFinal == null){
+                                                $query->where('espestadospendientes.espid', 0);
+                                            }else{
                                                 $query->where('fecfecha', $fechaFinal."-01");
-                                            // }
+                                            }
                                         })
                                         ->distinct('are.areid')
                                         ->get([
@@ -58,125 +55,45 @@ class MetMostrarEstadosPendientesController extends Controller
                                             'areporcentaje',
                                         ]);
 
-            $aresn = array(
-                array(),
-                array(),
-                array(),
-                array(),
-            );
+            if(sizeof($ares) > 0){
 
-            $fechaActual = date('Y-m-d');
-            $date1 = new DateTime($fechaActual);
-
-            foreach($ares as $posicionAre => $are){
-
-                $esps = espestadospendientes::join('perpersonas as per', 'per.perid', 'espestadospendientes.perid')
-                                            ->where('areid', $are->areid)
-                                            ->get([
-                                                'espfechaprogramado',
-                                                'espchacargareal',
-                                                'espfechactualizacion',
-                                                'espbasedato',
-                                                'espresponsable',
-                                                'espdiaretraso',
-                                                'pernombrecompleto',
-                                                'pernombre',
-                                                'perapellidopaterno',
-                                                'perapellidomaterno',
-                                            ]);
-
-                foreach($esps as $posicionEsp => $esp){
-                    
-                    $diaRetraso = $esp->espdiaretraso;
-
-                    if($esp->espfechactualizacion == null){
-
-                        $fecha_carga_real = date("Y-m-d", strtotime($esp->espfechaprogramado));
+                $aresn = array(
+                    array(),
+                    array(),
+                    array(),
+                    array(),
+                );
+    
+                $fechaActual = date('Y-m-d');
+                $date1 = new DateTime($fechaActual);
+    
+                foreach($ares as $posicionAre => $are){
+    
+                    $esps = espestadospendientes::join('perpersonas as per', 'per.perid', 'espestadospendientes.perid')
+                                                ->where('areid', $are->areid)
+                                                ->get([
+                                                    'espfechaprogramado',
+                                                    'espchacargareal',
+                                                    'espfechactualizacion',
+                                                    'espbasedato',
+                                                    'espresponsable',
+                                                    'espdiaretraso',
+                                                    'pernombrecompleto',
+                                                    'pernombre',
+                                                    'perapellidopaterno',
+                                                    'perapellidomaterno',
+                                                ]);
+    
+                    foreach($esps as $posicionEsp => $esp){
                         
-                        $date2 = new DateTime($fecha_carga_real);
-
-                        if($date1 > $date2){
-                            $diff = $date1->diff($date2);
-
-                            if($diff->days > 0){
-                                $diaRetraso = $diff->days;
-                            }else{
-                                $diaRetraso = "0";
-                            }
-
-                        }else{
-                            $diaRetraso = "0";
-                        }
-                    }
-
-                    $esps[$posicionEsp]['espdiaretraso'] = $diaRetraso;
-
-                }
-
-                $ares[$posicionAre]['esps'] = $esps;
-
-                if($are->arenombre == "SAC Sell Out Detalle"){
-                    $aresn[3] = $ares[$posicionAre];
-                }else if($are->arenombre == "SAC Sell Out"){
-                    $aresn[2] = $ares[$posicionAre];
-                }else if($are->arenombre == "SAC Sell In"){
-                    $aresn[1] = $ares[$posicionAre];
-                }else{
-                    $aresn[0] = $ares[$posicionAre];
-                }
-            }
-
-            // $tprs[$posicionTpr]['ares'] = $ares;
-            $tprs[$posicionTpr]['ares'] = $aresn;
-
-            if($posicionTpr == 0){
-                $tprs[$posicionTpr]['seleccionado'] = true; 
-            }
-
-
-            $espsDistribuidoras = espestadospendientes::join('perpersonas as per', 'per.perid', 'espestadospendientes.perid')
-                                                    ->join('areareasestados as are', 'are.areid', 'espestadospendientes.areid')
-                                                    ->join('tprtipospromociones as tpr', 'tpr.tprid', 'are.tprid')
-                                                    ->join('fecfechas as fec', 'fec.fecid', 'espestadospendientes.fecid')
-                                                    ->leftjoin('cliclientes as cli', 'cli.cliid', 'espestadospendientes.cliid')
-                                                    ->leftjoin('zonzonas as zon', 'zon.zonid', 'cli.zonid')
-                                                    ->where('are.arenombre', 'SAC Sell Out Detalle')
-                                                    ->where('tpr.tprid', $tpr->tprid)
-                                                    ->where(function ($query) use($fechaInicio, $fechaFinal) {
-                                                        $query->where('fecfecha', $fechaFinal."-01");
-                                                    })
-                                                    ->orderby('zonorden', 'desc')
-                                                    ->get([
-                                                        'espfechaprogramado',
-                                                        'espchacargareal',
-                                                        'espfechactualizacion',
-                                                        'espbasedato',
-                                                        'espresponsable',
-                                                        'espdiaretraso',
-                                                        'pernombrecompleto',
-                                                        'pernombre',
-                                                        'perapellidopaterno',
-                                                        'perapellidomaterno',
-                                                        'zon.zonnombre',
-                                                        'clihml',
-                                                        'clisuchml',
-                                                        'clitv',
-                                                        'cliclientesac'
-                                                    ]);
-
-            foreach($espsDistribuidoras as $posicionEsp => $esp){
-                    
-                    $diaRetraso = $esp->espdiaretraso;
-
-                    if($esp->espfechactualizacion == null){
-
-                        $fecha_carga_real = date("Y-m-d", strtotime($esp->espfechaprogramado));
-                        
-                        $date2 = new DateTime($fecha_carga_real);
-
-                        if($date1 == $date2){
-                            $diaRetraso = "0";
-                        }else{
+                        $diaRetraso = $esp->espdiaretraso;
+    
+                        if($esp->espfechactualizacion == null){
+    
+                            $fecha_carga_real = date("Y-m-d", strtotime($esp->espfechaprogramado));
+                            
+                            $date2 = new DateTime($fecha_carga_real);
+    
                             if($date1 > $date2){
                                 $diff = $date1->diff($date2);
     
@@ -190,30 +107,110 @@ class MetMostrarEstadosPendientesController extends Controller
                                 $diaRetraso = "0";
                             }
                         }
-                        
+    
+                        $esps[$posicionEsp]['espdiaretraso'] = $diaRetraso;
+    
                     }
+    
+                    $ares[$posicionAre]['esps'] = $esps;
+    
+                    if($are->arenombre == "SAC Sell Out Detalle"){
+                        $aresn[3] = $ares[$posicionAre];
+                    }else if($are->arenombre == "SAC Sell Out"){
+                        $aresn[2] = $ares[$posicionAre];
+                    }else if($are->arenombre == "SAC Sell In"){
+                        $aresn[1] = $ares[$posicionAre];
+                    }else{
+                        $aresn[0] = $ares[$posicionAre];
+                    }
+                }
+    
+                // $tprs[$posicionTpr]['ares'] = $ares;
+                $tprs[$posicionTpr]['ares'] = $aresn;
+    
+                if($posicionTpr == 0){
+                    $tprs[$posicionTpr]['seleccionado'] = true; 
+                }
+    
+    
+                $espsDistribuidoras = espestadospendientes::join('perpersonas as per', 'per.perid', 'espestadospendientes.perid')
+                                                        ->join('areareasestados as are', 'are.areid', 'espestadospendientes.areid')
+                                                        ->join('tprtipospromociones as tpr', 'tpr.tprid', 'are.tprid')
+                                                        ->join('fecfechas as fec', 'fec.fecid', 'espestadospendientes.fecid')
+                                                        ->leftjoin('cliclientes as cli', 'cli.cliid', 'espestadospendientes.cliid')
+                                                        ->leftjoin('zonzonas as zon', 'zon.zonid', 'cli.zonid')
+                                                        ->where('are.arenombre', 'SAC Sell Out Detalle')
+                                                        ->where('tpr.tprid', $tpr->tprid)
+                                                        ->where(function ($query) use($fechaInicio, $fechaFinal) {
+                                                            $query->where('fecfecha', $fechaFinal."-01");
+                                                        })
+                                                        ->orderby('zonorden', 'desc')
+                                                        ->get([
+                                                            'espfechaprogramado',
+                                                            'espchacargareal',
+                                                            'espfechactualizacion',
+                                                            'espbasedato',
+                                                            'espresponsable',
+                                                            'espdiaretraso',
+                                                            'pernombrecompleto',
+                                                            'pernombre',
+                                                            'perapellidopaterno',
+                                                            'perapellidomaterno',
+                                                            'zon.zonnombre',
+                                                            'clihml',
+                                                            'clisuchml',
+                                                            'clitv',
+                                                            'cliclientesac'
+                                                        ]);
+    
+                foreach($espsDistribuidoras as $posicionEsp => $esp){
+                        
+                        $diaRetraso = $esp->espdiaretraso;
+    
+                        if($esp->espfechactualizacion == null){
+    
+                            $fecha_carga_real = date("Y-m-d", strtotime($esp->espfechaprogramado));
+                            
+                            $date2 = new DateTime($fecha_carga_real);
+    
+                            if($date1 == $date2){
+                                $diaRetraso = "0";
+                            }else{
+                                if($date1 > $date2){
+                                    $diff = $date1->diff($date2);
+        
+                                    if($diff->days > 0){
+                                        $diaRetraso = $diff->days;
+                                    }else{
+                                        $diaRetraso = "0";
+                                    }
+        
+                                }else{
+                                    $diaRetraso = "0";
+                                }
+                            }
+                            
+                        }
+    
+                        $espsDistribuidoras[$posicionEsp]['espdiaretraso'] = $diaRetraso;
+    
+                }
 
-                    $espsDistribuidoras[$posicionEsp]['espdiaretraso'] = $diaRetraso;
-
+            }else{
+                $respuesta = false;
+                $mensaje   = "No se encontraron estados pendientes";
             }
-
         }
 
         $data = $tprs;
-
-
-
-
-
-
-
-
 
         $requestsalida = response()->json([
             "datos" => $data,
             "espsDistribuidoras" => $espsDistribuidoras,
             "date1" => $date1,
             "date2" => $date2,
+            "respuesta" => $respuesta,
+            "mensaje"   => $mensaje,
         ]);
 
         return $requestsalida;
