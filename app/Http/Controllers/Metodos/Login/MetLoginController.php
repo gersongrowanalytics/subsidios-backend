c0026af837d59d3b5a8b23a0c63a3866fca040fa
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-10-07 23:56:56 -0500

    login git push

--- app/Http/Controllers/Metodos/Login/MetLoginController.php
+++ app/Http/Controllers/Metodos/Login/MetLoginController.php
@@ -84,7 +84,7 @@ class MetLoginController extends Controller
             'datos'     => $datos,
             'fecha'     => $fechaDisponible,
             'subsidiospendientes' => true,
-            'fechaActualizacion' => "01 Octubre 2021",
+            'fechaActualizacion' => "07 Octubre 2021",
         ]);
     }
 }
