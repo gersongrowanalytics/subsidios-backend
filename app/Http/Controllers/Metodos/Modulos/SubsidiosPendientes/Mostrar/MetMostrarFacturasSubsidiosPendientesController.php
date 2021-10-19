7ad8f0a524778cbfada9e874b225273be98de623
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-10-19 18:31:09 -0500

    fact sub pendiente

--- app/Http/Controllers/Metodos/Modulos/SubsidiosPendientes/Mostrar/MetMostrarFacturasSubsidiosPendientesController.php
+++ app/Http/Controllers/Metodos/Modulos/SubsidiosPendientes/Mostrar/MetMostrarFacturasSubsidiosPendientesController.php
@@ -17,55 +17,77 @@ class MetMostrarFacturasSubsidiosPendientesController extends Controller
         $coddestinatariocuatro = $request['sdecodigodestinatario'];
         $coddestinatariocinco  = $request['sdecodigodestinatario'];
 
+        $tieneDestinatarios = true;
+
         if($coddestinatario == "170209"){
             $coddestinatario = "170418";
             $coddestinatariodos = "170418";
             $coddestinatariotres = "170418";
+            $tieneDestinatarios = true;
         }else if($coddestinatario == "278981"){
             $coddestinatario = "284861";
             $coddestinatariodos = "278982";
             $coddestinatariotres = "278981";
-
+            $tieneDestinatarios = true;
         }else if($coddestinatario == "96444"){
             $coddestinatario = "96474";
             $coddestinatariodos = "96474";
             $coddestinatariotres = "96474";
+            $tieneDestinatarios = true;
         }else if($coddestinatario == "287493"){
             $coddestinatario = "289634";
             $coddestinatariodos = "289634";
             $coddestinatariotres = "289634";
+            $tieneDestinatarios = true;
         }else if($coddestinatario == "143397"){
             $coddestinatario = "143398";
             $coddestinatariodos = "143398";
             $coddestinatariotres = "143398";
+            $tieneDestinatarios = true;
         }else if($coddestinatario == "160864"){
             $coddestinatario = "148921";
             $coddestinatariodos = "148921";
             $coddestinatariotres = "148921";
+            $tieneDestinatarios = true;
         }else if($coddestinatario == "166945"){
             $coddestinatario = "146628";
             $coddestinatariodos = "151379";
             $coddestinatariotres = "151379";
+            $tieneDestinatarios = true;
         }else if($coddestinatario == "144124"){
             $coddestinatario = "108016";
             $coddestinatariodos = "168098";
             $coddestinatariotres = "241834";
             $coddestinatariocuatro = "252650";
             $coddestinatariocinco  = "80133";
+            $tieneDestinatarios = true;
         }else if($coddestinatario == "130157"){
             $coddestinatario = "271678";
             $coddestinatariodos = "271678";
             $coddestinatariotres = "271678";
+            $tieneDestinatarios = true;
         }else if($coddestinatario == "54237"){
             $coddestinatario = "54239";
             $coddestinatariodos = "171802";
             $coddestinatariotres = "171802";
+            $tieneDestinatarios = true;
         }
 
         $fsis = fdsfacturassidetalles::join('fsifacturassi as fsi', 'fsi.fsiid', 'fdsfacturassidetalles.fsiid')
                                 ->join('fecfechas as fec', 'fec.fecid', 'fsi.fecid')
                                 ->join('proproductos as pro', 'pro.proid', 'fdsfacturassidetalles.proid')
-                                ->where('fsi.fsidestinatario', $coddestinatario)
+                                // ->where('fsi.fsidestinatario', $coddestinatario)
+                                ->where(function ($query) use($tieneDestinatarios, $coddestinatario, $coddestinatariodos, $coddestinatariotres, $coddestinatariocuatro, $coddestinatariocinco) {
+                                    if($tieneDestinatarios = true){
+                                        $query->orwhere('fsi.fsidestinatario', $coddestinatario);
+                                        $query->orwhere('fsi.fsidestinatario', $coddestinatariodos);
+                                        $query->orwhere('fsi.fsidestinatario', $coddestinatariotres);
+                                        $query->orwhere('fsi.fsidestinatario', $coddestinatariocuatro);
+                                        $query->orwhere('fsi.fsidestinatario', $coddestinatariocinco);
+                                    }else{
+                                        $query->where('fsi.fsidestinatario', $coddestinatario);
+                                    }
+                                })
                                 // ->orwhere('fsi.fsidestinatario', $coddestinatariodos)
                                 // ->orwhere('fsi.fsidestinatario', $coddestinatariotres)
                                 // ->orwhere('fsi.fsidestinatario', $coddestinatariocuatro)
