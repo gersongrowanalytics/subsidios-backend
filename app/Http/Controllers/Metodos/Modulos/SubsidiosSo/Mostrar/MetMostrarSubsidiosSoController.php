709646398c80ec5bf7ff925f0c05c265007e6395
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-10-27 00:51:32 -0500

    agrupacion

--- app/Http/Controllers/Metodos/Modulos/SubsidiosSo/Mostrar/MetMostrarSubsidiosSoController.php
+++ app/Http/Controllers/Metodos/Modulos/SubsidiosSo/Mostrar/MetMostrarSubsidiosSoController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\sdesubsidiosdetalles;
 use App\Models\zonzonas;
+use App\Models\cliclientes;
 use DB;
 
 class MetMostrarSubsidiosSoController extends Controller
