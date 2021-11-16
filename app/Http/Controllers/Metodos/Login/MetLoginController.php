fad512b997492c2b0faed932e20df69c9f12853c
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-11-16 09:04:34 -0500

    login

--- app/Http/Controllers/Metodos/Login/MetLoginController.php
+++ app/Http/Controllers/Metodos/Login/MetLoginController.php
@@ -111,7 +111,7 @@ class MetLoginController extends Controller
             'datos'     => $datos,
             'fecha'     => $fechaDisponible,
             'subsidiospendientes' => $subsidiospendientes,
-            'fechaActualizacion' => "12 Noviembre 2021",
+            'fechaActualizacion' => "15 Noviembre 2021",
             'mesespendientes' => $mesespendientes,
             'tiempo' => $tiempo,
         ]);
