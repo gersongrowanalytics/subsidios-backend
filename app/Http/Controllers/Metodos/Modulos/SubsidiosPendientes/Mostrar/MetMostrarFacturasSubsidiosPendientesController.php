508377d471a52f46af895be8155572a3f893113c
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-01-18 08:41:35 -0500

    s

--- app/Http/Controllers/Metodos/Modulos/SubsidiosPendientes/Mostrar/MetMostrarFacturasSubsidiosPendientesController.php
+++ app/Http/Controllers/Metodos/Modulos/SubsidiosPendientes/Mostrar/MetMostrarFacturasSubsidiosPendientesController.php
@@ -13,89 +13,89 @@ class MetMostrarFacturasSubsidiosPendientesController extends Controller
     public function MetMostrarFacturasSubsidiosPendientes(Request $request)
     {
         $coddestinatario       = $request['sdecodigodestinatario'];
-        $coddestinatariodos    = $request['sdecodigodestinatario'];
-        $coddestinatariotres   = $request['sdecodigodestinatario'];
-        $coddestinatariocuatro = $request['sdecodigodestinatario'];
-        $coddestinatariocinco  = $request['sdecodigodestinatario'];
+        // $coddestinatariodos    = $request['sdecodigodestinatario'];
+        // $coddestinatariotres   = $request['sdecodigodestinatario'];
+        // $coddestinatariocuatro = $request['sdecodigodestinatario'];
+        // $coddestinatariocinco  = $request['sdecodigodestinatario'];
 
-        $coddestinatarioseis   = $request['sdecodigodestinatario'];
-        $coddestinatariosiete  = $request['sdecodigodestinatario'];
-        $coddestinatarioocho   = $request['sdecodigodestinatario'];
-        $coddestinatarionueve  = $request['sdecodigodestinatario'];
-        $coddestinatariodiez   = $request['sdecodigodestinatario'];
-        $coddestinatarioonce   = $request['sdecodigodestinatario'];
+        // $coddestinatarioseis   = $request['sdecodigodestinatario'];
+        // $coddestinatariosiete  = $request['sdecodigodestinatario'];
+        // $coddestinatarioocho   = $request['sdecodigodestinatario'];
+        // $coddestinatarionueve  = $request['sdecodigodestinatario'];
+        // $coddestinatariodiez   = $request['sdecodigodestinatario'];
+        // $coddestinatarioonce   = $request['sdecodigodestinatario'];
 
-        $tieneDestinatarios = true;
+        // $tieneDestinatarios = true;
 
-        if($coddestinatario == "170209"){
-            $coddestinatario = "170418";
-            $coddestinatariodos = "170418";
-            $coddestinatariotres = "170418";
-            $tieneDestinatarios = true;
-        }else if($coddestinatario == "278981"){
-            $coddestinatario = "284861";
-            $coddestinatariodos = "278982";
-            $coddestinatariotres = "278981";
-            $tieneDestinatarios = true;
-        }else if($coddestinatario == "96444"){
-            $coddestinatario = "96474";
-            $coddestinatariodos = "96474";
-            $coddestinatariotres = "96474";
-            $tieneDestinatarios = true;
-        }else if($coddestinatario == "287493"){
-            $coddestinatario = "289634";
-            $coddestinatariodos = "289634";
-            $coddestinatariotres = "289634";
-            $tieneDestinatarios = true;
-        }else if($coddestinatario == "143397"){
-            $coddestinatario = "143398";
-            $coddestinatariodos = "143398";
-            $coddestinatariotres = "143398";
-            $tieneDestinatarios = true;
-        }else if($coddestinatario == "160864"){ //
-            $coddestinatario = "148921";
-            $coddestinatariodos = "148921";
-            $coddestinatariotres = "148921";
-            $tieneDestinatarios = true;
-        }else if($coddestinatario == "166945"){ //
-            $coddestinatario = "146628";
-            $coddestinatariodos = "151379";
-            $coddestinatariotres = "151379";
-            $tieneDestinatarios = true;
-        }else if($coddestinatario == "144124"){
-            $coddestinatario = "108016";
-            $coddestinatariodos = "168098";
-            $coddestinatariotres = "241834";
-            $coddestinatariocuatro = "252650";
-            $coddestinatariocinco  = "80133";
-            $tieneDestinatarios = true;
-        }else if($coddestinatario == "130157"){
-            $coddestinatario = "271678";
-            $coddestinatariodos = "271678";
-            $coddestinatariotres = "271678";
-            $tieneDestinatarios = true;
-        }else if($coddestinatario == "54237"){
-            $coddestinatario = "54239";
-            $coddestinatariodos = "171802";
-            $coddestinatariotres = "171802";
-            $tieneDestinatarios = true;
-        }else if($coddestinatario == "168098"){
+        // if($coddestinatario == "170209"){
+        //     $coddestinatario = "170418";
+        //     $coddestinatariodos = "170418";
+        //     $coddestinatariotres = "170418";
+        //     $tieneDestinatarios = true;
+        // }else if($coddestinatario == "278981"){
+        //     $coddestinatario = "284861";
+        //     $coddestinatariodos = "278982";
+        //     $coddestinatariotres = "278981";
+        //     $tieneDestinatarios = true;
+        // }else if($coddestinatario == "96444"){
+        //     $coddestinatario = "96474";
+        //     $coddestinatariodos = "96474";
+        //     $coddestinatariotres = "96474";
+        //     $tieneDestinatarios = true;
+        // }else if($coddestinatario == "287493"){
+        //     $coddestinatario = "289634";
+        //     $coddestinatariodos = "289634";
+        //     $coddestinatariotres = "289634";
+        //     $tieneDestinatarios = true;
+        // }else if($coddestinatario == "143397"){
+        //     $coddestinatario = "143398";
+        //     $coddestinatariodos = "143398";
+        //     $coddestinatariotres = "143398";
+        //     $tieneDestinatarios = true;
+        // }else if($coddestinatario == "160864"){ //
+        //     $coddestinatario = "148921";
+        //     $coddestinatariodos = "148921";
+        //     $coddestinatariotres = "148921";
+        //     $tieneDestinatarios = true;
+        // }else if($coddestinatario == "166945"){ //
+        //     $coddestinatario = "146628";
+        //     $coddestinatariodos = "151379";
+        //     $coddestinatariotres = "151379";
+        //     $tieneDestinatarios = true;
+        // }else if($coddestinatario == "144124"){
+        //     $coddestinatario = "108016";
+        //     $coddestinatariodos = "168098";
+        //     $coddestinatariotres = "241834";
+        //     $coddestinatariocuatro = "252650";
+        //     $coddestinatariocinco  = "80133";
+        //     $tieneDestinatarios = true;
+        // }else if($coddestinatario == "130157"){
+        //     $coddestinatario = "271678";
+        //     $coddestinatariodos = "271678";
+        //     $coddestinatariotres = "271678";
+        //     $tieneDestinatarios = true;
+        // }else if($coddestinatario == "54237"){
+        //     $coddestinatario = "54239";
+        //     $coddestinatariodos = "171802";
+        //     $coddestinatariotres = "171802";
+        //     $tieneDestinatarios = true;
+        // }else if($coddestinatario == "168098"){
             
-            $coddestinatario = "168098";
-            $coddestinatariodos = "108016";
-            $coddestinatariotres = "144124";
-            $coddestinatariocuatro = "241834";
-            $coddestinatariocinco  = "252649";
+        //     $coddestinatario = "168098";
+        //     $coddestinatariodos = "108016";
+        //     $coddestinatariotres = "144124";
+        //     $coddestinatariocuatro = "241834";
+        //     $coddestinatariocinco  = "252649";
 
-            $coddestinatarioseis  = "252650";
-            $coddestinatariosiete  = "143070";
-            $coddestinatarioocho  = "116986";
-            $coddestinatarionueve  = "138113";
-            $coddestinatariodiez  = "270535";
-            $coddestinatarioonce  = "80133";
+        //     $coddestinatarioseis  = "252650";
+        //     $coddestinatariosiete  = "143070";
+        //     $coddestinatarioocho  = "116986";
+        //     $coddestinatarionueve  = "138113";
+        //     $coddestinatariodiez  = "270535";
+        //     $coddestinatarioonce  = "80133";
 
-            $tieneDestinatarios = true;
-        }
+        //     $tieneDestinatarios = true;
+        // }
 
         $cli = cliclientes::where('clicodigoshipto', $coddestinatario)->first();
 
