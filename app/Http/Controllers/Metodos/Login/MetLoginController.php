acec69c5690720a70df21d6a032b0ae2d02cdbda
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-02-11 08:51:32 -0500

    login act

--- app/Http/Controllers/Metodos/Login/MetLoginController.php
+++ app/Http/Controllers/Metodos/Login/MetLoginController.php
@@ -33,7 +33,7 @@ class MetLoginController extends Controller
                             ->where('usuusuario', $usuario)
                             ->first([
                                 'usuid',
-                                'usu.estid',
+                                'usuusuarios.estid',
                                 'per.perid',
                                 'pernumerodocumentoidentidad',
                                 'pernombrecompleto',
