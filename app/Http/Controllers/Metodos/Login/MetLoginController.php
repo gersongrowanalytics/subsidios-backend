aa0ce00e5b960a2ba6d4e1187acca79ab8886e8a
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-02-11 08:48:08 -0500

    login act

--- app/Http/Controllers/Metodos/Login/MetLoginController.php
+++ app/Http/Controllers/Metodos/Login/MetLoginController.php
@@ -53,53 +53,58 @@ class MetLoginController extends Controller
                             ]);
 
         if($usu){
-            if (Hash::check($contrasenia, $usu->usucontrasenia)) {
+            if($usu->estid == 1){
+                if (Hash::check($contrasenia, $usu->usucontrasenia)) {
 
-                $fechaDisponible = fecfechas::where('fecmesabierto', true)->first();
-
-                $tuptiposusuariospermisos = tuptiposusuariospermisos::join('pempermisos as pem', 'pem.pemid', 'tuptiposusuariospermisos.pemid')
-                                                                    ->where('tuptiposusuariospermisos.tpuid', $usu->tpuid )
-                                                                    ->get([
-                                                                        'tuptiposusuariospermisos.tupid',
-                                                                        'pem.pemnombre',
-                                                                        'pem.pemslug'
-                                                                    ]);
-
-                if(sizeof($tuptiposusuariospermisos) > 0){
-                    $usu->permisos = $tuptiposusuariospermisos;
-                }else{
-                    $usu->permisos = [];
-                }
-
-                $mensaje = "Bienvenido, ".$usuario." es un gusto volver a verte por aquí";
-                $datos = $usu;
-
-                $mesespendientes = [];
-
-                $sdes = sdesubsidiosdetalles::join('fecfechas as fec', 'fec.fecid', 'sdesubsidiosdetalles.fecid')
-                                            ->where('sdependiente', true)
-                                            ->where('fec.fecid', '!=', 1104)
-                                            ->distinct('sdesubsidiosdetalles.fecid')
-                                            ->get([
-                                                'sdesubsidiosdetalles.fecid',
-                                                'fec.fecmesabreviacion',
-                                                'fec.fecanionumero',
-                                            ]);
-
-                if(sizeof($sdes) > 0){
-                    $subsidiospendientes = true;
-                    foreach($sdes as $sde){
-                        $mesespendientes[] = array(
-                            "anio" => $sde->fecmesabreviacion,
-                            "mes"  => $sde->fecanionumero
-                        );
+                    $fechaDisponible = fecfechas::where('fecmesabierto', true)->first();
+    
+                    $tuptiposusuariospermisos = tuptiposusuariospermisos::join('pempermisos as pem', 'pem.pemid', 'tuptiposusuariospermisos.pemid')
+                                                                        ->where('tuptiposusuariospermisos.tpuid', $usu->tpuid )
+                                                                        ->get([
+                                                                            'tuptiposusuariospermisos.tupid',
+                                                                            'pem.pemnombre',
+                                                                            'pem.pemslug'
+                                                                        ]);
+    
+                    if(sizeof($tuptiposusuariospermisos) > 0){
+                        $usu->permisos = $tuptiposusuariospermisos;
+                    }else{
+                        $usu->permisos = [];
                     }
-                    $tiempo = $sdes;
+    
+                    $mensaje = "Bienvenido, ".$usuario." es un gusto volver a verte por aquí";
+                    $datos = $usu;
+    
+                    $mesespendientes = [];
+    
+                    $sdes = sdesubsidiosdetalles::join('fecfechas as fec', 'fec.fecid', 'sdesubsidiosdetalles.fecid')
+                                                ->where('sdependiente', true)
+                                                ->where('fec.fecid', '!=', 1104)
+                                                ->distinct('sdesubsidiosdetalles.fecid')
+                                                ->get([
+                                                    'sdesubsidiosdetalles.fecid',
+                                                    'fec.fecmesabreviacion',
+                                                    'fec.fecanionumero',
+                                                ]);
+    
+                    if(sizeof($sdes) > 0){
+                        $subsidiospendientes = true;
+                        foreach($sdes as $sde){
+                            $mesespendientes[] = array(
+                                "anio" => $sde->fecmesabreviacion,
+                                "mes"  => $sde->fecanionumero
+                            );
+                        }
+                        $tiempo = $sdes;
+                    }
+    
+                }else{
+                    $respuesta = false;
+                    $mensaje = "Lo sentimos, el usuario o contraseña es incorrecta";
                 }
-
             }else{
                 $respuesta = false;
-                $mensaje = "Lo sentimos, el usuario o contraseña es incorrecta";
+                $mensaje = "Lo sentimos, el usuario se encuentra actualmente desactivado";
             }
         }else{
             $respuesta = false;
